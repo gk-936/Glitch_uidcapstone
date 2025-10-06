b9d852980ee061b9a89bab3734a3ed62933d6738
gk-936 <ggokul0273@gmail.com>
2025-10-06 18:17:14 +0530

    dark mode fixed

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -10,7 +10,11 @@ export const useTheme = (): [Theme, (theme: Theme) => void] => {
 
   useEffect(() => {
     localStorage.setItem('theme', theme);
-    document.documentElement.setAttribute('data-theme', theme);
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }, [theme]);
 
   const setTheme = (newTheme: Theme) => {
